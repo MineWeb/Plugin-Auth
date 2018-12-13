edf71b26b3f62860dccf7bd12382f8861f6cfbe5
Kuro <8905889+KuroSenseii@users.noreply.github.com>
2018-12-12 19:10:19 -0500

    fix default value colomn

--- SQL/schema.php
+++ SQL/schema.php
@@ -10,8 +10,8 @@ class AuthAppSchema extends CakeSchema {
         public function after($event = array()) {}
 
         public $users = [
-            'auth-uuid' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-            'auth-accessToken' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-            'auth-clientToken' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1')
+            'auth-uuid' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+            'auth-accessToken' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+            'auth-clientToken' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1')
         ];
 }
